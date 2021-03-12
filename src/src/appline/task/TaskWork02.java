9a7bf0e032c6aa84a9abfa4d3ec1445f249a914d
ArtMarenkov <sc48@yandex.ru>
2021-03-12 17:58:10 +0300

    Создан класс TaskWork02.

---
+++ src/src/appline/task/TaskWork02.java
@@ -0,0 +1,18 @@
+package src.appline.task;
+
+public class TaskWork02
+{
+
+    public static void main(String[] args)
+    {
+
+    double d;
+    d = 3.14;
+    System.out.println(d);
+
+    byte b;
+    b = 127;
+    System.out.println(b);
+
+    }
+}
